28626479e1a6baa8e875bf55cc1c032984a3507c
Tatiana <florentseva.design@gmail.com>
2026-01-02 09:52:34 +0700

    правки 10

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sri } from 'vite-plugin-sri'
+import { subresourceIntegrity } from 'vite-plugin-sri'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    sri() 
+    subresourceIntegrity()
   ],
 })

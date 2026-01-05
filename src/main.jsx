37f9e35e1364640d898fc77cc47bcbf111b71a2e
Tatiana <florentseva.design@gmail.com>
2026-01-05 16:45:57 +0700

    аналитика добавлена

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { LocaleProvider } from "./context/LocaleContext";
+import { Analytics } from "@vercel/analytics/react";
 
 import App from "./App.jsx";
 import "./styles/base.css";
@@ -18,6 +19,7 @@ createRoot(document.getElementById("root")).render(
     <HelmetProvider>
       <LocaleProvider>
         <App />
+        <Analytics />
       </LocaleProvider>
     </HelmetProvider>
   </StrictMode>
